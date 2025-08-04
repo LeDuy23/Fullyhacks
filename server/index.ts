376e64ba93b048d2f8731214b844126bf8f84e60
LeDuy23 <143040215+LeDuy23@users.noreply.github.com>
2025-08-04 01:22:38 -0700

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -50,11 +50,8 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+  // Always serve static files from docs directory
+  serveStatic(app);
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
@@ -68,3 +65,4 @@ app.use((req, res, next) => {
     log(`serving on port ${port}`);
   });
 })();
+
